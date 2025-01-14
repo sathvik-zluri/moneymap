843b95d67bab5a0d38fa51669a8049e1304b2d71
sathvik-zluri <sathvik.k@zluri.com>
2025-01-14 19:11:28 +0530

    Chore: update .gitignore to include coverage files and modify jest.config for test matching and TypeScript config

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,11 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.spec.ts'],
+  testMatch: ['<rootDir>/tests/**/*.test.ts', '**/__tests__/**/*.test.ts', '**/__tests__/**/*.spec.ts'],
   collectCoverage: true,
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json', // Specify TypeScript config
-    },
+    "^.+\\.tsx?$": ["ts-jest", {
+      tsconfig: '<rootDir>/tsconfig.json', // Specify TypeScript config
+    }],
   },
 };
