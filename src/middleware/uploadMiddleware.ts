cb106dc92394157809c05c0ec6149f746008d684
sathvik-zluri <sathvik.k@zluri.com>
2025-01-16 08:56:09 +0530

    feat: simplify file upload middleware by removing error handling

--- src/middleware/uploadMiddleware.ts
+++ src/middleware/uploadMiddleware.ts
@@ -1,4 +1,4 @@
-import multer, { MulterError } from "multer";
+import multer from "multer";
 import { Request, Response, NextFunction } from "express";
 
 // Configure Multer storage to use memory
@@ -7,11 +7,7 @@ const upload = multer({ storage });
 
 // Middleware to handle file upload
 export const uploadMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  upload.single("file")(req, res, (err: MulterError | any) => {
-    if (err) {
-        console.error("Error uploading file:", err.message);
-        return res.status(400).json({ error: "No file uploaded" });
-    }
+  upload.single("file")(req, res, () => {
     next();
   });
 };
