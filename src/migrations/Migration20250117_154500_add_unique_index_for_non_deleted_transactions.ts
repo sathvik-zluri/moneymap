bd7037a2afc49d2ce9323d72c9e41becc4e610b1
sathvik-zluri <sathvik.k@zluri.com>
2025-01-17 16:49:32 +0530

    feat: update transaction amount type to numeric and add partial unique index for non-deleted transactions

---
+++ src/migrations/Migration20250117_154500_add_unique_index_for_non_deleted_transactions.ts
@@ -0,0 +1,17 @@
+import { Migration } from "@mikro-orm/migrations";
+
+export class Migration20250117_154500 extends Migration {
+  async up(): Promise<void> {
+    // Create a partial unique index for transactions where `isDeleted` is false
+    this.addSql(`
+          CREATE UNIQUE INDEX unique_transaction_not_deleted
+          ON "transction" ("date", "description")
+          WHERE "is_deleted" = false;
+        `);
+  }
+
+  async down(): Promise<void> {
+    // Drop the partial unique index if rolled back
+    this.addSql("DROP INDEX IF EXISTS unique_transaction_not_deleted;");
+  }
+}
