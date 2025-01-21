8c743f18f7bc8c214ec2af2d8a95172ce6f5f85c
sathvik-zluri <sathvik.k@zluri.com>
2025-01-21 17:49:33 +0530

    feat: update transaction schema to include amount_inr and change date type to date

--- src/migrations/Migration20250120_154500_add_unique_index_for_non_deleted_transactions.ts
+++ src/migrations/Migration20250120_154500_add_unique_index_for_non_deleted_transactions.ts
@@ -4,7 +4,7 @@ export class Migration20250117_154500 extends Migration {
   async up(): Promise<void> {
     // Create a partial unique index for transactions where `isDeleted` is false
     this.addSql(`
-          CREATE UNIQUE INDEX unique_transaction_not_deleted
+          CREATE UNIQUE INDEX unique_transction_not_deleted
           ON "transction" ("date", "description")
           WHERE "is_deleted" = false;
         `);
